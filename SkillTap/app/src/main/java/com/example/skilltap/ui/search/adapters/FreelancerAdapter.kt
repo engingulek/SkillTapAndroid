496604d27f66cc54f34ef1a5470f838d098bf9e5
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2024-12-08 23:05:28 +0300

    finished user info card view on freelancer design

--- SkillTap/app/src/main/java/com/example/skilltap/ui/search/adapters/FreelancerAdapter.kt
+++ SkillTap/app/src/main/java/com/example/skilltap/ui/search/adapters/FreelancerAdapter.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.skilltap.R
 import com.example.skilltap.databinding.AdvertAdapterDesignBinding
 import com.example.skilltap.databinding.FreelancerAdapterDesingBinding
+import com.example.skilltap.ui.search.SearchFragmentDirections
+import com.example.skilltap.utils.toFragment
 
 class FreelancerAdapter(var mContext: Context)
     : RecyclerView.Adapter<FreelancerAdapter.FreelancerAdapterKeeper>() {
@@ -32,5 +35,9 @@ class FreelancerAdapter(var mContext: Context)
 
     override fun onBindViewHolder(holder: FreelancerAdapterKeeper, position: Int) {
         holder.design.nameSurnmaTxt.text = "Name Surname"
+        holder.design.freelancerCardView.setOnClickListener {
+            val nav = SearchFragmentDirections.toFreelancerDetail()
+            Navigation.toFragment(it,nav)
+        }
     }
 }
