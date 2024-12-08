2a76fe0f5e1785dc301cea10dce48f798f4bd990
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2024-12-08 23:28:21 +0300

    finished freelancer detail design

--- SkillTap/app/src/main/java/com/example/skilltap/ui/freelancerDetail/FreelancerDetailFragment.kt
+++ SkillTap/app/src/main/java/com/example/skilltap/ui/freelancerDetail/FreelancerDetailFragment.kt
@@ -4,21 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.skilltap.R
-
+import com.example.skilltap.databinding.FragmentFreelancerDetailBinding
 
 
 class FreelancerDetailFragment : Fragment() {
+    private lateinit var  design : FragmentFreelancerDetailBinding
+    private lateinit var viewModel:FreelancerDetailViewModelInterface
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_freelancer_detail, container, false)
+        design = DataBindingUtil.inflate(inflater,R.layout.fragment_freelancer_detail, container, false)
+        val ownerAdvertAdapter = OwnerAdvertAdapter(requireContext())
+        design.ownerAdvertAdapter = ownerAdvertAdapter
+        viewModel.uiState.observe(viewLifecycleOwner){
+            design.uiState = it
+        }
+        design.ownerAdvertsRv.layoutManager =  LinearLayoutManager(requireContext(),
+            LinearLayoutManager.HORIZONTAL,false)
+        return design.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tempViewModel : FreelancerDetailViewModelInterface by viewModels<FreelancerDetailViewModel>()
+        viewModel = tempViewModel
+    }
 
 }
---
+++ SkillTap/app/src/main/java/com/example/skilltap/ui/freelancerDetail/OwnerAdvertAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.skilltap.ui.freelancerDetail
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.skilltap.R
+import com.example.skilltap.databinding.AdvertAdapterDesignBinding
+import com.example.skilltap.ui.search.SearchFragmentDirections
+import com.example.skilltap.utils.toFragment
+
+class OwnerAdvertAdapter(var mContext: Context)
+    :RecyclerView.Adapter<OwnerAdvertAdapter.OwnerAdapterDesignKeeper>(){
+        inner class OwnerAdapterDesignKeeper(var design : AdvertAdapterDesignBinding)
+            :RecyclerView.ViewHolder(design.root){
+
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OwnerAdapterDesignKeeper {
+        val layoutInflater = LayoutInflater.from(mContext)
+        val design: AdvertAdapterDesignBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.advert_adapter_design,
+            parent,
+            false
+        )
+
+        return  OwnerAdapterDesignKeeper(design)
+    }
+
+    override fun getItemCount(): Int {
+        return  10
+    }
+
+    override fun onBindViewHolder(holder: OwnerAdapterDesignKeeper, position: Int) {
+        holder.design.titleTxt.text = "Test Title"
+        holder.design.advertCardView.setOnClickListener {
+            val nav = SearchFragmentDirections.toAdvertDetail()
+            Navigation.toFragment(it,nav)
+        }
+    }
+}
